3221e20f277d223ba7dcdaa17be7f1debcce08c0
Tim Smith <tim.smith2814@gmail.com>
2023-08-03 09:53:10 +0100

    Fix width issue

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,25 @@
 function embiggen() {
   // the div the effects the size of the breakout room menu
   const theBox = document.querySelector(".R3Gmyc");
+  const borBoxes = document.querySelectorAll(".fRqe8");
 
   // either increase the size or unset the inline style
   if (!theBox.classList.contains("bigboye")) {
-    theBox.style.width = "98%";
     theBox.classList.add("bigboye");
+    theBox.style.width = "98%";
+
+    // make the breakout room boxes half size
+    for (let i = 0; i < borBoxes.length; i++) {
+      borBoxes[i].style.width = "48%";
+      borBoxes[i].style.display = "inline-block";
+    }
   } else {
     theBox.style.width = "";
     theBox.classList.remove("bigboye");
+    for (let i = 0; i < borBoxes.length; i++) {
+      borBoxes[i].style.width = "";
+      borBoxes[i].style.display = "";
+    }
   }
 }
 
